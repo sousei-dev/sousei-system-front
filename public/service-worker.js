32e17512a00b4d3830416a259f8fa980b3f94962
sousei-dev <dev@sousei-group.com>
2025-09-29 14:36:13 +0900

    push 테스트

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 self.addEventListener("push", event => {
+  alert('push')
   const data = event.data ? event.data.json() : {};
   const title = data.title || "새 메시지";
   const options = {
     body: data.body || "메시지가 도착했습니다!",
-    icon: "/icon.png",
-    badge: "/badge.png"
+    icon: "/pwa-192x192.png",
+    badge: "/pwa-192x192.png"
   };
   event.waitUntil(self.registration.showNotification(title, options));
 });
